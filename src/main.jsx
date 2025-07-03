4decd3efa680b078bbe970d9d6435a9429d7500f
dattpse182773 <dattpse182773@fpt.edu.vn>
2025-07-03 08:36:08 +0700

    feat: integrate Sentry for error tracking and performance monitoring
    - Added Sentry dependencies to package.json
    - Initialized Sentry in main.jsx with DSN and configuration for tracing
    - Configured Sentry Vite plugin in vite.config.js for better integration
    - Enabled source maps in build configuration for improved debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import { ToastContainer } from 'react-toastify'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { ToastContainer } from 'react-toastify';
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://b0c5fbd5c57d27383f10ce71a02a843d@o4509587694616576.ingest.de.sentry.io/4509587827982416",
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  sendDefaultPii: true,
+});
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <ToastContainer />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(), sentryVitePlugin({
+    org: "fpt-university-7m",
+    project: "test-swp"
+  })],
+
   optimizeDeps: {
     include: ['@ant-design/icons'],
   },
+
+  build: {
+    sourcemap: true
+  }
 })
